767aee4d2402a4aa1a72a38e7e6af466f843b15a
tcronis <46980360+tcronis@users.noreply.github.com>
2019-04-13 16:32:01 -0500

    Data changes

--- src/main/java/MainFolder/Main.java
+++ src/main/java/MainFolder/Main.java
@@ -11,9 +11,9 @@ import org.neuroph.core.data.*;
 public class Main {
     public static void main( String[] args ) {
         // create new perceptron network
-        NeuralNetwork neuralNetwork = new Perceptron(2, 1);
+        NeuralNetwork neuralNetwork = new Perceptron(2, 1); //two inputs one ouput
         // create training set
-        DataSet trainingSet = new DataSet(2, 1);
+        DataSet trainingSet = new DataSet(2, 1);    //two inputs one ouput
         // add training data to training set (logical OR function)
         trainingSet. addRow (new DataSetRow (new double[]{0, 0},
         new double[]{0}));
@@ -36,7 +36,6 @@ public class Main {
         neuralNetwork2.calculate();
         // get network output
         double[] networkOutput = neuralNetwork2.getOutput();
-        
         
         //AccessDatabase ad = new AccessDatabase();
         //ad.connectToDatabase();
